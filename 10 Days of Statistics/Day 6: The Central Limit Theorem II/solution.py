f251b2614b1190b329f3f25e7e9ebd2624afb014
MarehanRefaat <72582389+MarehanRefaat@users.noreply.github.com>
2020-11-10 23:30:01 +0200

    Create solution.py

---
+++ 10 Days of Statistics/Day 6: The Central Limit Theorem II/solution.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+import math
+x = int(input())
+n = int(input())
+mean = float(input())
+var = float(input())
+m  = mean * n
+v = var * math.sqrt(n)
+cdf = lambda l : 0.5 + 0.5 * math.erf((l-m)/(v* 2**0.5))
+print("%.4f"%cdf(x))
